3b96942811848e46ee0e3d226f9ad1e81fb5b371
Ross Hetherton <rosshetherton@live.com>
2021-12-30 16:04:18 +1100

    updated styling to allow mobile responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,7 @@ export default function Header() {
                 <h6 className="contact"><a href="google.com">Contact</a></h6>
                 <h6 className="bookings"><a href="google.com">Bookings</a></h6>
             </div>
+            <button className="menu--icon"></button>
         </div>
     )
 }
